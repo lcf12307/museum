caa4246c6f80d9c9d3777696f7245322eb3a54c7
lcf12307 <lcf12307@sina.com>
2018-01-08 23:30:14 +0800

    total222333

--- src/main/java/com/crtvu/dto/FinalPoint.java
+++ src/main/java/com/crtvu/dto/FinalPoint.java
@@ -5,17 +5,311 @@ public class FinalPoint {
     private String name;
     private int mid;
     private int year;
-    private int point;
-    private int point1;
-    private int point11;
-    private int point12;
-    private int point13;
-    private int point14;
-    private int point2;
-    private int point21;
-    private int point22;
-    private int point23;
-    private int point24;
-    private int point25;
+    private double point;
+    private double point1;
+    private double point11;
+    private double point12;
+    private double point13;
+    private double point14;
+    private double point101;
+    private double point102;
+    private double point103;
+    private double point104;
+    private double point105;
+    private double point106;
+    private double point107;
 
+
+    private double point108;
+    private double point109;
+
+    public double getPoint109() {
+        return point109;
+    }
+
+    public void setPoint109(double point109) {
+        this.point109 = point109;
+    }
+
+    private double point2;
+    private double point21;
+    private double point22;
+    private double point23;
+    private double point24;
+    private double point25;
+    public void init(){
+        this.setPoint(0);
+        this.setPoint1(0);
+        this.setPoint2(0);
+        this.setPoint11(0);
+        this.setPoint12(0);
+        this.setPoint13(0);
+        this.setPoint14(0);
+        this.setPoint101(0);
+        this.setPoint102(0);
+        this.setPoint103(0);
+        this.setPoint104(0);
+        this.setPoint105(0);
+        this.setPoint106(0);
+        this.setPoint107(0);
+        this.setPoint108(0);
+        this.setPoint109(0);
+        this.setPoint21(0);
+        this.setPoint22(0);
+        this.setPoint23(0);
+        this.setPoint24(0);
+        this.setPoint25(0);
+    }
+    public FinalPoint(int id, String name, int mid, int year, double point, double point1, double point11, double point12, double point13, double point14, double point101, double point102, double point103, double point104, double point105, double point106, double point107, double point108, double point109, double point2, double point21, double point22, double point23, double point24, double point25) {
+        this.id = id;
+        this.name = name;
+        this.mid = mid;
+        this.year = year;
+        this.point = point;
+        this.point1 = point1;
+        this.point11 = point11;
+        this.point12 = point12;
+        this.point13 = point13;
+        this.point14 = point14;
+        this.point101 = point101;
+        this.point102 = point102;
+        this.point103 = point103;
+        this.point104 = point104;
+        this.point105 = point105;
+        this.point106 = point106;
+        this.point107 = point107;
+        this.point108 = point108;
+        this.point109 = point109;
+        this.point2 = point2;
+        this.point21 = point21;
+        this.point22 = point22;
+        this.point23 = point23;
+        this.point24 = point24;
+        this.point25 = point25;
+    }
+    @Override
+    public String toString() {
+        return "FinalPoint{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", mid=" + mid +
+                ", year=" + year +
+                ", point=" + point +
+                ", point1=" + point1 +
+                ", point11=" + point11 +
+                ", point12=" + point12 +
+                ", point13=" + point13 +
+                ", point14=" + point14 +
+                ", point101=" + point101 +
+                ", point102=" + point102 +
+                ", point103=" + point103 +
+                ", point104=" + point104 +
+                ", point105=" + point105 +
+                ", point106=" + point106 +
+                ", point107=" + point107 +
+                ", point108=" + point108 +
+                ", point2=" + point2 +
+                ", point21=" + point21 +
+                ", point22=" + point22 +
+                ", point23=" + point23 +
+                ", point24=" + point24 +
+                ", point25=" + point25 +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMid() {
+        return mid;
+    }
+
+    public void setMid(int mid) {
+        this.mid = mid;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPoint() {
+        return point;
+    }
+
+    public void setPoint(double point) {
+        this.point = point;
+    }
+
+    public double getPoint1() {
+        return point1;
+    }
+
+    public void setPoint1(double point1) {
+        this.point1 = point1;
+    }
+
+    public double getPoint11() {
+        return point11;
+    }
+
+    public void setPoint11(double point11) {
+        this.point11 = point11;
+    }
+
+    public double getPoint12() {
+        return point12;
+    }
+
+    public void setPoint12(double point12) {
+        this.point12 = point12;
+    }
+
+    public double getPoint13() {
+        return point13;
+    }
+
+    public void setPoint13(double point13) {
+        this.point13 = point13;
+    }
+
+    public double getPoint14() {
+        return point14;
+    }
+
+    public void setPoint14(double point14) {
+        this.point14 = point14;
+    }
+
+    public double getPoint101() {
+        return point101;
+    }
+
+    public void setPoint101(double point101) {
+        this.point101 = point101;
+    }
+
+    public double getPoint102() {
+        return point102;
+    }
+
+    public void setPoint102(double point102) {
+        this.point102 = point102;
+    }
+
+    public double getPoint103() {
+        return point103;
+    }
+
+    public void setPoint103(double point103) {
+        this.point103 = point103;
+    }
+
+    public double getPoint104() {
+        return point104;
+    }
+
+    public void setPoint104(double point104) {
+        this.point104 = point104;
+    }
+
+    public double getPoint105() {
+        return point105;
+    }
+
+    public void setPoint105(double point105) {
+        this.point105 = point105;
+    }
+
+    public double getPoint106() {
+        return point106;
+    }
+
+    public void setPoint106(double point106) {
+        this.point106 = point106;
+    }
+
+    public double getPoint107() {
+        return point107;
+    }
+
+    public void setPoint107(double point107) {
+        this.point107 = point107;
+    }
+
+    public double getPoint108() {
+        return point108;
+    }
+
+    public void setPoint108(double point108) {
+        this.point108 = point108;
+    }
+
+    public double getPoint2() {
+        return point2;
+    }
+
+    public void setPoint2(double point2) {
+        this.point2 = point2;
+    }
+
+    public double getPoint21() {
+        return point21;
+    }
+
+    public void setPoint21(double point21) {
+        this.point21 = point21;
+    }
+
+    public double getPoint22() {
+        return point22;
+    }
+
+    public void setPoint22(double point22) {
+        this.point22 = point22;
+    }
+
+    public double getPoint23() {
+        return point23;
+    }
+
+    public void setPoint23(double point23) {
+        this.point23 = point23;
+    }
+
+    public double getPoint24() {
+        return point24;
+    }
+
+    public void setPoint24(double point24) {
+        this.point24 = point24;
+    }
+
+    public double getPoint25() {
+        return point25;
+    }
+
+    public void setPoint25(double point25) {
+        this.point25 = point25;
+    }
+
+    public FinalPoint() {
+
+    }
 }
--- src/main/java/com/crtvu/web/PointController.java
+++ src/main/java/com/crtvu/web/PointController.java
@@ -1,6 +1,7 @@
 package com.crtvu.web;
 
 import com.crtvu.dto.DeleteJson;
+import com.crtvu.dto.FinalPoint;
 import com.crtvu.entity.AttachmentEntity;
 import com.crtvu.entity.PointEntity;
 import com.crtvu.service.AttachmentService;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -50,6 +52,14 @@ public class PointController {
     public String quantitative(@RequestParam(value = "year") int year,
                     HttpServletRequest request){
         List<AttachmentEntity> attachments = attachmentService.findByYear(year,1);
+        List<FinalPoint> finalpoints = new ArrayList<>();
+        FinalPoint finalPoint = new FinalPoint();
+        finalPoint.init();
+        float[] maxRes = new float[100];
+        float[] sumRes = new float[100];
+        float[][] tempRes = new float[300][100];
+        int rowindex=0;
+        double point1=0,point11=0,point12=0,point13=0,point14=0,pointtemp=0;
         for (AttachmentEntity attachment : attachments){
             String dir = attachment.getFile();
             File file = new File(dir);
@@ -62,31 +72,105 @@ public class PointController {
                 if (file1.matches(".*博物馆运行评估申报书\\.doc(x)?$")){
                     System.out.println(file1);
                     float[] res = POI_Word.readData(dir+"\\"+file1);
-                    int index=0; PointEntity  point = new PointEntity();
+                    int index=0,index1=0; PointEntity  point = new PointEntity();
                     if ( pointService.findPointByName(attachment.getName(),0,year) == null){
                         pointService.addPoint(attachment.getName(),0,year,0,0);
-                        point = pointService.findPointByName(attachment.getName(),0,year).get(0);
+
                     }
-                    int point1=0,point11=0,point12=0,point13=0,point14=0;
+                    point = pointService.findPointByName(attachment.getName(),0,year).get(0);
                     if (pointService.findPointByName(attachment.getName(),1,year) != null){
                         pointService.deleteByYear(year);
                     }
+                    index1=0;index=0;
                     for (float temp :res){
-                        pointService.addPoint(attachment.getName(),point.getId(),year, temp*p[index],index+100);
-                        point1+=temp*p[index];
                         switch (index){
-                            case 0:
-                            case 1: point11+=temp*p[index];
-
-
-
+                            case 0:pointtemp=temp*p[index];finalPoint.setPoint101(pointtemp);break;
+                            case 1: pointtemp=temp*p[index];finalPoint.setPoint102(pointtemp);
+                                if (maxRes[index1]<pointtemp){
+                                    maxRes[index1] = (float) pointtemp;
+                                }
+                                sumRes[index1] += pointtemp;index1++; break;
+                            case 2: pointtemp = temp*p[index];break;
+                            case 3:pointtemp += temp*p[index];break;
+                            case 4:pointtemp += temp*p[index];finalPoint.setPoint103(pointtemp);
+                                if (maxRes[index1]<pointtemp){
+                                    maxRes[index1] = (float) pointtemp;
+                                }
+                                sumRes[index1] += pointtemp;index1++;break;
+                            case 5:pointtemp=temp*p[index];break;
+                            case 6:pointtemp+=temp*p[index];break;
+                            case 7:pointtemp+=temp*p[index];break;
+                            case 8:pointtemp+=temp*p[index];break;
+                            case 9:pointtemp+=temp*p[index];break;
+                            case 10:pointtemp+=temp*p[index];finalPoint.setPoint104(pointtemp);
+                                if (maxRes[index1]<pointtemp){
+                                    maxRes[index1] = (float) pointtemp;
+                                }
+                                sumRes[index1] += pointtemp;index1++;break;
+                            case 11:pointtemp=temp*p[index];break;
+                            case 12:pointtemp=temp*p[index];finalPoint.setPoint105(pointtemp);
+                                if (maxRes[index1]<pointtemp){
+                                    maxRes[index1] = (float) pointtemp;
+                                }
+                                sumRes[index1] += pointtemp;index1++;break;
+                            case 13:pointtemp=temp*p[index];break;
+                            case 14:pointtemp+=temp*p[index];break;
+                            case 15:pointtemp+=temp*p[index];break;
+                            case 16:pointtemp+=temp*p[index];break;
+                            case 17:pointtemp+=temp*p[index];finalPoint.setPoint106(pointtemp);
+                                if (maxRes[index1]<pointtemp){
+                                    maxRes[index1] = (float) pointtemp;
+                                }
+                                sumRes[index1] += pointtemp;index1++;break;
+                            case 18:pointtemp=temp*p[index];break;
+                            case 19:pointtemp+=temp*p[index];break;
+                            case 20:pointtemp+=temp*p[index];break;
+                            case 21:pointtemp+=temp*p[index];break;
+                            case 22:pointtemp+=temp*p[index];break;
+                            case 23:pointtemp+=temp*p[index];finalPoint.setPoint107(pointtemp);
+                                if (maxRes[index1]<pointtemp){
+                                    maxRes[index1] = (float) pointtemp;
+                                }
+                                sumRes[index1] += pointtemp;index1++;break;
+                            case 24:pointtemp=temp*p[index];break;
+                            case 25:pointtemp+=temp*p[index];finalPoint.setPoint108(pointtemp);
+                                if (maxRes[index1]<pointtemp){
+                                    maxRes[index1] = (float) pointtemp;
+                                }
+                                sumRes[index1] += pointtemp;index1++;break;
+                            case 26:pointtemp=temp*p[index];break;
+                            case 27:pointtemp+=temp*p[index];finalPoint.setPoint109(pointtemp);
+                                if (maxRes[index1]<pointtemp){
+                                    maxRes[index1] = (float) pointtemp;
+                                }
+                                sumRes[index1] += pointtemp;index1++;
                         }
                         index++;
                     }
+                    finalpoints.add(finalPoint);
+                    finalPoint.init();
                     break;
                 }
             }
         }
+            for (FinalPoint newpoint : finalpoints){
+                newpoint.setPoint101(0.1*((((newpoint.getPoint101()-(sumRes[0]/finalpoints.size()))*0.4)/(maxRes[0] - (sumRes[0]/finalpoints.size())))+0.6));
+                newpoint.setPoint102(0.05*((((newpoint.getPoint102()-(sumRes[1]/finalpoints.size()))*0.4)/(maxRes[1] - (sumRes[1]/finalpoints.size())))+0.6));
+                newpoint.setPoint103(0.08*((((newpoint.getPoint103()-(sumRes[2]/finalpoints.size()))*0.4)/(maxRes[2] - (sumRes[2]/finalpoints.size())))+0.6));
+                newpoint.setPoint104(0.12*((((newpoint.getPoint104()-(sumRes[3]/finalpoints.size()))*0.4)/(maxRes[3] - (sumRes[3]/finalpoints.size())))+0.6));
+                newpoint.setPoint105(0.05*((((newpoint.getPoint105()-(sumRes[4]/finalpoints.size()))*0.4)/(maxRes[4] - (sumRes[4]/finalpoints.size())))+0.6));
+                newpoint.setPoint106(0.25*((((newpoint.getPoint106()-(sumRes[5]/finalpoints.size()))*0.4)/(maxRes[5] - (sumRes[5]/finalpoints.size())))+0.6));
+                newpoint.setPoint107(0.20*((((newpoint.getPoint107()-(sumRes[6]/finalpoints.size()))*0.4)/(maxRes[6] - (sumRes[6]/finalpoints.size())))+0.6));
+                newpoint.setPoint108(0.05*((((newpoint.getPoint108()-(sumRes[7]/finalpoints.size()))*0.4)/(maxRes[7] - (sumRes[7]/finalpoints.size())))+0.6));
+                newpoint.setPoint109(0.1 *((((newpoint.getPoint109()-(sumRes[7]/finalpoints.size()))*0.4)/(maxRes[7] - (sumRes[7]/finalpoints.size())))+0.6));
+                newpoint.setPoint11(newpoint.getPoint101()+newpoint.getPoint102());
+                newpoint.setPoint12(newpoint.getPoint103()+newpoint.getPoint104()+newpoint.getPoint105());
+                newpoint.setPoint13(newpoint.getPoint106()+newpoint.getPoint107());
+                newpoint.setPoint14(newpoint.getPoint108()+newpoint.getPoint109());
+                newpoint.setPoint1(newpoint.getPoint11()+newpoint.getPoint12()+newpoint.getPoint13()+newpoint.getPoint14());
+            }
+
+
         return "index";
     }
 }
